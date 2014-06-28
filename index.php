3726b48624b2d8398ec67a5c71941f2f2674eb21
Laura Filman <lmfilman@gmail.com>
2014-06-28 19:01:55 -0400

    adding index.php & image

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<h1>P1 - Laura Filman - Dynamic Web Applications</h1>
+<img src="images/harvardid.jpg">
+<h2>Questions:</h2>
+<h3>In 2+ sentences describe who you are and what you do.</h3>
+	<p>This is my answer</p>
+<h3>Why are you taking this course?</h3>
+	<p>This is my answer</p>
+<h3>What previous experience do you have with HTML/CSS?</h3>
+	<p>This is my answer</p>
+<h3>What previous experience do you have with programming?</h3>
+	<p>This is my answer</p>
+<h3>Have you worked with PHP before? If so, to what extent?</h3>
+	<p>This is my answer</p>
+<h3>Are you working on a Mac, PC, or some other platform?</h3>
+	<p>This is my answer</p>
+<h3>Anything else we should know?</h3>
+	<p>This is my answer</p>
