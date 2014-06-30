845e691db0471ac5641b6ea93116e78bfdbc1e1c
Laura Filman <lmfilman@gmail.com>
2014-06-30 00:02:40 -0400

    added answers to website questions

--- index.php
+++ index.php
@@ -10,19 +10,19 @@
 			<img src="images/harvardid.jpg" alt="Laura" title="Laura">
 			<h2>Questions:</h2>
 			<h3>In 2+ sentences describe who you are and what you do.</h3>
-				<p>This is my answer</p>
+				<p>Hi! My name is Laura, and I'm a QE (quality engineer) at MathWorks (they make MATLAB). I primarily work on verifying and writing automated tests for the web applications on <a href="http://www.mathworks.com/matlabcentral/">MATLAB Central</a>.  In my spare time, I like to jog, cook, and watch Netflix.</p>
 			<h3>Why are you taking this course?</h3>
-				<p>This is my answer</p>
+				<p>I'm taking Dynamic Web Applications because I'd like to gain more experience with web applications from a developer's perspective, rather than as a QE.  I'd like to gain a better understanding of the whole system and how all the pieces fit together.</p>
 			<h3>What previous experience do you have with HTML/CSS?</h3>
-				<p>This is my answer</p>
+				<p>I have limited experience with HTML/CSS.  I have never written it myself before now.</p>
 			<h3>What previous experience do you have with programming?</h3>
-				<p>This is my answer</p>
+				<p>I have solid experience with programming. I received my B.A. in Computer Science in 2013, and I use my programming knowledge on a day-to-day basis writing automated tests in Java.</p>
 			<h3>Have you worked with PHP before? If so, to what extent?</h3>
-				<p>This is my answer</p>
+				<p>I worked with PHP <em>once</em>, using my knowledge of other programming languages to help my mom debug a couple issues on her PHP-based web site.</p>
 			<h3>Are you working on a Mac, PC, or some other platform?</h3>
-				<p>This is my answer</p>
+				<p>I am working on a PC.</p>
 			<h3>Anything else we should know?</h3>
-				<p>This is my answer</p>
+				<p>Not that I can think of :)</p>
 		</div>
 	</body>
 
